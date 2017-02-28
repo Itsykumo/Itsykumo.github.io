7c212e5328544b09fa17f04870193401ddb6caf2
Sean Su <itsykumo@gmail.com>
2017-02-27 22:50:01 -0800

    add hover to logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 particlesJS.load('particles-js', '/assets/js/particlesjs-config.json');
+
+$('#left-navbar > .logo').mouseover(function () {
+  $('body').addClass('body-hover');
+}).mouseout(function () {
+  $('body').removeClass('body-hover');
+});
